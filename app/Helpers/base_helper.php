1932d33e9de2541c13df3684ce36bf4b543f2dd8
ali niaz <ali.niaz@finja.pk>
2020-07-07 20:56:33 +0500

    soap client/server both in working

---
+++ app/Helpers/base_helper.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ali Niaz
+ * Date: 7/7/2020
+ * Time: 8:02 PM
+ */
+
+if (!function_exists('test')) {
+    function test($input){
+        return $input . ', die man!';
+    }
+}
